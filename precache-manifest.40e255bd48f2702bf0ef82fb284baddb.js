3dc2467d935773bea517909cba65ddee1902d9fc
Andrew Sova <andrii.sova@tbwa-x.com>
2020-10-13 16:10:27 +0300

    Updates

---
+++ precache-manifest.40e255bd48f2702bf0ef82fb284baddb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fcba005fa1b1f63ce2c98c03c716014e",
+    "url": "/render-previews/index.html"
+  },
+  {
+    "revision": "e29ea15e6db93d4a69e6",
+    "url": "/render-previews/static/css/main.cbfe79a8.chunk.css"
+  },
+  {
+    "revision": "1fc7463290346e7891b1",
+    "url": "/render-previews/static/js/2.436b4b71.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/render-previews/static/js/2.436b4b71.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e29ea15e6db93d4a69e6",
+    "url": "/render-previews/static/js/main.1a553ec3.chunk.js"
+  },
+  {
+    "revision": "2c9c06421fdad611fca4",
+    "url": "/render-previews/static/js/runtime-main.341a2270.js"
+  }
+]);
